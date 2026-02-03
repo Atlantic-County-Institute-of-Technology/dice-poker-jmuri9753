18902823798fcc19b053c439628033622ecccdb8
jm524 <muri9753@student.acitech.org>
2026-02-03 10:34:43 -0500

    Did some comments

--- die.py
+++ die.py
@@ -1,7 +1,7 @@
 # Creator: Jayden Murillo
 # Project: Dice Poker
 # Made: 1.13.26
-# Last Edit: 2.1.26
+# Last Edit: 2.2.26
 
 import random
 
@@ -10,25 +10,24 @@ class Dice:
 
     def __init__(self):
         self.MAX_DICE = 5
-        self.dice = []
+        self.dice = []  # Creates a dice object with lists inside it, like a list of dies and a list or keeping or reolling
         self.store = []
 
         for i in range(self.MAX_DICE):
-            self.dice.append(Die())
+            self.dice.append(Die())  # Adds the die and the keeping or rolling to the dice objects lists
             self.store.append("Rerolling")
 
 
 class Die:
-    
 
     def __init__(self):
-        self.faces = 6
+        self.faces = 6 # Die object with 6 faces, and a value. This is for each individual dice
         self.value = 1
         
 
     def roll_dice(self):
         self.value = random.randint(1,self.faces)
-        return self.value
+        return self.value  # This rolls the dice and changes it value to be a number 1-6.
     
     def get_value(self):
-        return self.faces
+        return self.faces # Returns number of faces but isn't used in dice poker code
--- multiplayer.py
+++ multiplayer.py
@@ -1,7 +1,7 @@
 # Creator: Jayden Murillo
 # Made: 1.13.26
-# Last Edit: 2.1.26
+# Last Edit: 2.2.26
 
-players = [True,True]
-player_names = ["Player 1", "Player 2"]
-player_scores = [0,0]
+players = [True,True] # Number of players In a List, True for each player
+player_names = ["Player 1", "Player 2"] # List of each player's saved name accordingly to the player's index
+player_scores = [0,0] # Player scores, which all start at 0 until the users play
